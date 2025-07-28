57d544aa354d408427c8226ed9d44f8fe3abe7d3
Samuel Yvon <samuelyvon9@gmail.com>
2025-07-27 21:51:01 -0400

    Maths

--- src/physics/mod.rs
+++ src/physics/mod.rs
@@ -1,7 +1,7 @@
 pub mod euler;
 
-use raylib::drawing::RaylibDrawHandle;
 use crate::body::Body;
+use raylib::drawing::RaylibDrawHandle;
 
 /// Gravity constant
 pub const G: f32 = 6.6674 * 1E-11;
@@ -14,7 +14,7 @@ pub trait Kinematics {
 
     /// Hook to draw information about the kinematics. Can be used to draw paths of
     /// objects.
-    fn draw(&self, drawing_handle : &mut RaylibDrawHandle);
+    fn draw(&self, drawing_handle: &mut RaylibDrawHandle);
 }
 
 /// Compute the euclidian distance between two bodies. Returns
@@ -64,4 +64,55 @@ pub fn update_acceleration(bodies: &mut [Body]) {
 
         bodies[i].accel = (x_acc, y_acc);
     }
+}
+
+/// Given an angle in radians, a radius (a distance in meters), computes the position of the second
+/// body relative to the one at [pos], assuming it is on a circle around the first body.
+pub fn orient(theta: f32, radius: f32, pos: (f32, f32)) -> (f32, f32) {
+    let dy = theta.sin() * radius;
+    let dx = theta.cos() * radius;
+    let (x, y) = pos;
+    (x + dx, y + dy)
+}
+
+/// Parameters to create an orbit
+pub struct OrbitParameters {
+    /// Semi-major axis, meters
+    pub a: f32,
+    /// Eccentricity
+    pub e: f32,
+    /// True anomaly, radians
+    pub theta: f32,
+}
+
+/// Configure an orbit between two bodies. 
+/// 
+/// Warning: 
+/// Overwrites the position of the bodies, as well as their velocities.
+/// This means you should set the mass of the bodies and place them both 
+/// anywhere, then configure the orbit.
+pub fn kepler_orbit(orb: OrbitParameters, body1: &mut Body, body2: &mut Body) {
+    let mu = G * (body1.mass + body2.mass);
+
+    let p = orb.a * (1. - orb.e.powf(2.0));
+
+    // Position
+    let r = (orb.a * p) / (1. + orb.e * orb.theta.cos());
+    let (rx, ry) = (orb.theta.cos() * r, orb.theta.sin() * r);
+
+    // Velocity
+    let factor = (mu / p).sqrt();
+    let (vx, vy) = (
+        -orb.theta.sin() * factor,
+        (orb.theta.cos() + orb.e) * factor,
+    );
+
+    let m1_mt = (body1.mass) / (body1.mass + body2.mass);
+    let m2_mt = (body2.mass) / (body1.mass + body2.mass);
+
+    body1.set_pos((-m2_mt * rx, -m2_mt * ry));
+    body2.set_pos((m1_mt * rx, m1_mt * ry));
+
+    body1.velocity = (-m2_mt * vx, -m2_mt * vy);
+    body2.velocity = (m1_mt * vx, m1_mt * vy);
 }
